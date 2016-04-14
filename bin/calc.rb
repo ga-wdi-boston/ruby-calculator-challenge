10a47b4a795f70577a2af6957b19f1c4283050e5
sjf125 <sjf125@gmail.com>
2016-04-14 09:38:43 -0400

    Complete ruby-calculator-challenge

--- bin/calc.rb
+++ bin/calc.rb
@@ -1,6 +1,31 @@
 require 'pry'
 
 # Calculator Class here
+class Calc
+  def initialize(num)
+    @num = num
+  end
+
+  def plus(num)
+    @num = @num + num
+    self
+  end
+
+  def minus(num)
+    @num = @num - num
+    self
+  end
+
+  def times(num)
+    @num = @num * num
+    self
+  end
+
+  def divided_by(num)
+    @num = @num / num
+    self
+  end
+end
 
 binding.pry
 ''
