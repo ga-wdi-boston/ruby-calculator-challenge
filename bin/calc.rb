7fc123514c4df28804f942782c78b7faae52fb67
soynog <nicolas.o.garcia@gmail.com>
2016-04-14 09:15:08 -0400

    Ruby Calculator Challenge Response

--- bin/calc.rb
+++ bin/calc.rb
@@ -1,6 +1,33 @@
 require 'pry'
 
 # Calculator Class here
+class Calculator
+  attr_accessor :x
+
+  def initialize(x = 0)
+    @x = x.to_f
+  end
+
+  def plus(y)
+    puts @x += y
+    self
+  end
+
+  def minus(y)
+    puts @x -= y
+    self
+  end
+
+  def times(y)
+    puts @x *= y
+    self
+  end
+
+  def div_by(y)
+    puts @x /= y
+    self
+  end
+end
 
 binding.pry
 ''
