359c35629fdab32d82f395ce4059f09f7ca79d44
shkherad <shkherad@bu.edu>
2016-06-10 09:18:41 -0400

    calculator challenge
    completed with bonus

--- bin/calc.rb
+++ bin/calc.rb
@@ -1,6 +1,34 @@
 require 'pry'
 
-# Calculator Class here
+class Calculator
+  attr_accessor :curr
+  def initialize(curr)
+    @curr = curr
+  end
+
+  def add(num)
+    @curr += num
+    return self
+  end
+
+  def subtract(num)
+    @curr -= num
+    return self
+  end
+
+  def multiply(num)
+    @curr *= num
+    return self
+  end
+
+  def divide(num)
+    @curr /= num
+    return self
+  end
+end
+
+puts Calculator.new(5).add(3).multiply(2).subtract(1).divide(3)
+
 
 binding.pry
 ''
