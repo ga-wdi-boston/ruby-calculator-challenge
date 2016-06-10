43a548a2f8ad7028c1f39ab29989464e82241ab0
MishaHerscu <mishaherscu@gmail.com>
2016-06-10 09:18:26 -0400

    allows method chaining now

--- bin/calc.rb
+++ bin/calc.rb
@@ -4,22 +4,27 @@ require 'pry'
 class Calculator
   def initialize(input)
     @value = input
+    @is_integer = input.is_a? Integer
   end
 
   def add(num)
     @value += num
+    self
   end
 
   def subtract(num)
     @value -= num
+    self
   end
 
   def multiply(num)
     @value *= num
+    self
   end
 
   def divide(num)
     @value /= num
+    self
   end
 end
 
