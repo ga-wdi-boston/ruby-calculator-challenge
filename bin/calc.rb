643e98a2020c6eda60e0f562842fbdd3b36f0a75
MishaHerscu <mishaherscu@gmail.com>
2016-06-10 09:23:47 -0400

    track datatype

--- bin/calc.rb
+++ bin/calc.rb
@@ -5,25 +5,38 @@ class Calculator
   def initialize(input)
     @value = input
     @is_integer = input.is_a? Integer
+    @is_float = input.is_a? Float
   end
 
   def add(num)
     @value += num
+    @is_integer = @value.is_a? Integer
+    @is_float = @value.is_a? Float
+    p @value
     self
   end
 
   def subtract(num)
     @value -= num
+    @is_integer = @value.is_a? Integer
+    @is_float = @value.is_a? Float
+    p @value
     self
   end
 
   def multiply(num)
     @value *= num
+    @is_integer = @value.is_a? Integer
+    @is_float = @value.is_a? Float
+    p @value
     self
   end
 
   def divide(num)
     @value /= num
+    @is_integer = @value.is_a? Integer
+    @is_float = @value.is_a? Float
+    p @value
     self
   end
 end
