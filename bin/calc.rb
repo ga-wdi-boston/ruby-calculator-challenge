445307b7e8b024dfa7d20c8cc92998cf0407b9ca
esthercordova <estherpiacordova@gmail.com>
2016-06-10 09:27:34 -0400

    calculator

--- bin/calc.rb
+++ bin/calc.rb
@@ -1,6 +1,42 @@
 require 'pry'
 
-# Calculator Class here
+class Calculator
+#intitialize with two starter variables
+  def initialize(a,b)
+        @a = a
+        @b = b
+  end
+
+  def add
+   puts @a + @b
+  end
+
+  def subtract
+    puts @a - @b
+  end
+
+  def multiply
+    puts @a * @b
+  end
+
+  def divide
+    puts @a / @b
+  end
+
+end
+
+#make  new instances
+addition = Calculator.new(1,1)
+subtraction = Calculator.new(1,1)
+multiplication = Calculator.new(2,4)
+division = Calculator.new(9,3)
+
+#use the just created instances and make use of the method created in the class
+subtraction.subtract
+addition.add
+multiplication.multiply
+division.divide
+
 
 binding.pry
 ''
