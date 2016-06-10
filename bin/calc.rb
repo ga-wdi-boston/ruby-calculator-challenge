fc5083142be361c3b9b50160155be41cd61191b1
MishaHerscu <mishaherscu@gmail.com>
2016-06-10 09:10:48 -0400

    first draft

--- bin/calc.rb
+++ bin/calc.rb
@@ -1,6 +1,27 @@
 require 'pry'
 
 # Calculator Class here
+class Calculator
+  def initialize(input)
+    @value = input
+  end
+
+  def add(num)
+    @value += num
+  end
+
+  def subtract(num)
+    @value -= num
+  end
+
+  def multiply(num)
+    @value *= num
+  end
+
+  def divide(num)
+    @value /= num
+  end
+end
 
 binding.pry
 ''
