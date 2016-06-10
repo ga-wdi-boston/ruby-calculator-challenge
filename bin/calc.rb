ffad2c27f8e5c9ff395a45a9feae8d005ab9bc06
SarahHartwick <sarah@surfsetfitness.com>
2016-06-10 09:14:54 -0400

    Complete Challenge with Bonus
    Created Calculator class with method chaining

--- bin/calc.rb
+++ bin/calc.rb
@@ -2,5 +2,31 @@ require 'pry'
 
 # Calculator Class here
 
+class Calculator
+  def initialize(num)
+    @num = num
+  end
+
+  def add(num2)
+    @num += num2
+    self
+  end
+
+  def subtract(num3)
+    @num -= num3
+    self
+  end
+
+  def multiply(num4)
+    @num *= num4
+    self
+  end
+
+  def divide(num5)
+    @num /= num5
+    self
+  end
+end
+
 binding.pry
 ''
