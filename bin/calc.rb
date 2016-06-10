21f48f3338ebd50ef1b194175815e5059d886d4a
MishaHerscu <mishaherscu@gmail.com>
2016-06-10 09:35:01 -0400

    just storing one variable with datatype
    i thought we should do this because the prompt said to be aware of datatypes.
    that said, i also know that i can always just get the type from @value so this
    var is a bit unnecessary.

--- bin/calc.rb
+++ bin/calc.rb
@@ -4,38 +4,33 @@ require 'pry'
 class Calculator
   def initialize(input)
     @value = input
-    @is_integer = input.is_a? Integer
-    @is_float = input.is_a? Float
+    @value_class = @value.class
   end
 
   def add(num)
     @value += num
-    @is_integer = @value.is_a? Integer
-    @is_float = @value.is_a? Float
+    @value_class = @value.class
     p @value
     self
   end
 
   def subtract(num)
     @value -= num
-    @is_integer = @value.is_a? Integer
-    @is_float = @value.is_a? Float
+    @value_class = @value.class
     p @value
     self
   end
 
   def multiply(num)
     @value *= num
-    @is_integer = @value.is_a? Integer
-    @is_float = @value.is_a? Float
+    @value_class = @value.class
     p @value
     self
   end
 
   def divide(num)
     @value /= num
-    @is_integer = @value.is_a? Integer
-    @is_float = @value.is_a? Float
+    @value_class = @value.class
     p @value
     self
   end
