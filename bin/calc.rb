5b835d33fa696c2360b9aed9c11d0e1143735b5f
dvdpwll <dvdpwll456@gmail.com>
2016-08-04 09:28:04 -0400

    David Powell, challenge
    success!

--- bin/calc.rb
+++ bin/calc.rb
@@ -1,6 +1,36 @@
 require 'pry'
 
 # Calculator Class here
+class Calculator
+  # attr_accessor :num2
+  def initialize(num1)
+    @num1 = num1.to_f
+  end
+
+  def a(num2)
+    @num1 += num2
+    self
+    # p @num1
+  end
+
+  def s(num2)
+    @num1 -= num2
+    # p @num1
+    self
+  end
+
+  def m(num2)
+    @num1 *= num2
+    # p @num1
+    self
+  end
+
+  def d(num2)
+    @num1 /= num2
+    # p @num1
+    self
+  end
+end
 
 binding.pry
 ''
