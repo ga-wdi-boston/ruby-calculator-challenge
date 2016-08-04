7de23b6476393db007e04798b7fd121e2aedc40b
dvdpwll <dvdpwll456@gmail.com>
2016-08-04 09:39:46 -0400

    David Powell, challenge
    cleaned up code

--- bin/calc.rb
+++ bin/calc.rb
@@ -2,32 +2,31 @@ require 'pry'
 
 # Calculator Class here
 class Calculator
-  # attr_accessor :num2
   def initialize(num1)
     @num1 = num1.to_f
   end
 
+  # Addition
   def a(num2)
     @num1 += num2
     self
-    # p @num1
   end
 
+  # Subtraciton
   def s(num2)
     @num1 -= num2
-    # p @num1
     self
   end
 
+  # Multiply
   def m(num2)
     @num1 *= num2
-    # p @num1
     self
   end
 
+  # Divide
   def d(num2)
     @num1 /= num2
-    # p @num1
     self
   end
 end
