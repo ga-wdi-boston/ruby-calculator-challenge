3cb9e36756deae559c39419f17e29a695b578c06
rebekahheacock <rebekah.heacock@gmail.com>
2016-09-19 09:14:59 -0400

    Set up basic methods
    Define Calculator class
    Set up initialize, value=, add, subtract, multiply, and divide methods

--- bin/calc.rb
+++ bin/calc.rb
@@ -1,6 +1,38 @@
 require 'pry'
 
 # Calculator Class here
+class Calculator
+  attr_reader :value
+
+  def initialize(value)
+    @value = value
+  end
+
+  def value=(new_value)
+    @value = new_value
+    puts value
+  end
+
+  def add(num)
+    self.value += num
+    self
+  end
+
+  def subtract(num)
+    self.value -= num
+    self
+  end
+
+  def multiply(num)
+    self.value *= num
+    self
+  end
+
+  def divide(num)
+    self.value /= num
+    self
+  end
+end
 
 binding.pry
 ''
