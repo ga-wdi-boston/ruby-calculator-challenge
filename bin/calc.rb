abfa1a1fee8db69130beac55c919642bbbe1e249
rebekahheacock <rebekah.heacock@gmail.com>
2016-09-19 09:29:39 -0400

    Add rudimentary fixnum/float handling
    Check if number is whole, and convert to integer

--- bin/calc.rb
+++ bin/calc.rb
@@ -13,23 +13,33 @@ class Calculator
     puts value
   end
 
+  def convert_type(num)
+    if num == num.to_i
+      self.value = value.to_i
+    end
+  end
+
   def add(num)
     self.value += num
+    convert_type(self.value)
     self
   end
 
   def subtract(num)
     self.value -= num
+    convert_type(self.value)
     self
   end
 
   def multiply(num)
     self.value *= num
+    convert_type(self.value)
     self
   end
 
   def divide(num)
     self.value /= num
+    convert_type(self.value)
     self
   end
 end
