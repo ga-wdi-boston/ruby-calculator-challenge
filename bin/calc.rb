e82ef1be352b25a03a4f21fc1c8d35c6e0ad4439
sujkid <suja.sreekumar@yahoo.com>
2016-11-10 18:55:26 -0500

    Ruby Calculator
    Done.

--- bin/calc.rb
+++ bin/calc.rb
@@ -1,6 +1,27 @@
 require 'pry'
 
-# Calculator Class here
+# Calculator
+class Calculator
+  def initialize(num)
+    @num = num
+  end
 
-binding.pry
-''
+  def add(add_num)
+    @num + add_num
+  end
+
+  def multiply(multiply_num)
+    @num * multiply_num
+  end
+
+  def subtract(sub_num)
+    @num - sub_num
+  end
+
+  def divide(div_num)
+    @num / div_num.to_f
+  end
+end
+
+# binding.pry
+# ''
