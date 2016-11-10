da922eb72cda73fa13241bcff2a994811fbbf633
kibs86 <kibs086@gmail.com>
2016-11-10 10:06:38 -0500

    Update calc.rb for calculator challenge
    - update calc.rb with response to ruby calculator challenge

--- bin/calc.rb
+++ bin/calc.rb
@@ -1,6 +1,38 @@
 require 'pry'
 
 # Calculator Class here
+class Calculator
+  attr_reader :num1
+
+  def initialize(num1)
+    @result = num1
+    # @num1 = num1
+  end
+
+  def add(num2)
+    # @num2 = num2
+    @result += num2
+    self
+  end
+
+  def subtract(num2)
+    # @num2 = num2
+    @result -= num2
+    self
+  end
+
+  def multiply(num2)
+    # @num2 = num2
+    @result *= num2
+    self
+  end
+
+  def divide(num2)
+    # @num2 = num2
+    @result /= num2.to_f
+    self
+  end
+end
 
 binding.pry
 ''
