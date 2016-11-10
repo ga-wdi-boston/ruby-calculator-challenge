7fa26f4ae21c69ec60a828e856a4f35eca3ce9ce
ioncrash <ioncrash@gmail.com>
2016-11-10 09:13:36 -0500

    response to ruby challenge
    now bonus?

--- bin/calc.rb
+++ bin/calc.rb
@@ -1,6 +1,30 @@
 require 'pry'
 
-# Calculator Class here
+#calc for challenge
+class Calculator
+  attr_reader :add, :subtract, :multiply, :divide
+
+  def initialize(input)
+    @input = input.to_f
+  end
+
+  def add(val)
+    @input + val
+  end
+
+  def subtract(val)
+    @input - val
+  end
+
+  def multiply(val)
+    @input * val
+  end
+
+  def divide(val)
+    @input / val
+  end
+
+end
 
 binding.pry
 ''
