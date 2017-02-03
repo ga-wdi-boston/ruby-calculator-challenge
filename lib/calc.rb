0de19783b77c96af4e470edc4c442c9a6b903983
jwaskow <thewasko@gmail.com>
2017-02-03 09:31:02 -0500

    Submit Ruby Calculator Challenge

--- lib/calc.rb
+++ lib/calc.rb
@@ -1,3 +1,33 @@
 # frozen_string_literal: true
 
 # Calculator Class here
+
+class Calculator
+  def initialize(num1)
+    @num1 = num1
+  end
+
+  def add(num2)
+    @num1 += num2
+  end
+
+  def subtract(num2)
+    @num1 -= num2
+  end
+
+  def multtiply(num2)
+    @num1 *= num2
+  end
+
+  def divide(num2)
+    @num1 /= num2.to_f
+  end
+
+  def clear
+    @num1 = 0
+  end
+
+  def equals
+    p @num1
+  end
+end
