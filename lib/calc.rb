63c02a00e91c4d39f8aa167a7ca85cdf06499907
Sofistication <sordonez93@gmail.com>
2017-02-03 09:29:39 -0500

    Challenge requirements

--- lib/calc.rb
+++ lib/calc.rb
@@ -1,3 +1,39 @@
 # frozen_string_literal: true
 
 # Calculator Class here
+class Calculator
+  attr_accessor :number
+
+  def initialize(num)
+    @number = num.to_f
+  end
+
+  def add(num_two)
+    @number += num_two.to_f
+    @number
+  end
+
+  def subtract(num_two)
+    @number -= num_two.to_f
+    @number
+  end
+
+  def multiply(num_two)
+    @number *= num_two.to_f
+    @number
+  end
+
+  def divide(num_two)
+    @number /= num_two.to_f
+    @number
+  end
+
+  def equals
+    @number
+  end
+
+  def clear
+    @number = 0.to_f
+    @number
+  end
+end
