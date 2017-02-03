ae7c1ef6418af5968e590ce3ebe995cd61e4affd
“newayliu1” <newayliu1@gmail.com>
2017-02-03 09:32:35 -0500

    Complete challenge
    - Complete challenge without bonus

--- lib/calc.rb
+++ lib/calc.rb
@@ -1,3 +1,34 @@
 # frozen_string_literal: true
 
 # Calculator Class here
+class Calculator
+  attr_reader :start_num
+  def initialize(number)
+    @start_num = number
+    @result = number
+  end
+
+  def add(number)
+    @result = start_num + number
+  end
+
+  def minus(number)
+    @result = start_num - number
+  end
+
+  def times(number)
+    @result = start_num * number
+  end
+
+  def divide(number)
+    @result = start_num.to_f / number
+  end
+
+  def equals
+    @result
+  end
+
+  def clear
+    @result = 0
+  end
+end
