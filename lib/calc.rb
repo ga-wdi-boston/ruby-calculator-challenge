ec31ebb023aaaeff207b45fdb92daffdb3012226
dore.maxime@gmail.com <dore.maxime@gmail.com>
2017-02-03 09:29:24 -0500

    does not work running Calculator.addition(5, 3) error message: NoMethodError: undefined method `addition' for Calculator:Class from (pry):3:in `__pry__'

--- lib/calc.rb
+++ lib/calc.rb
@@ -1,3 +1,33 @@
 # frozen_string_literal: true
 
 # Calculator Class here
+class Calculator
+  attr_accessor :addition, :subtraction, :multiplication, :division
+
+  def initialize(starting_number, second_number)
+    @starting_number = starting_number
+    @second_number = second_number
+  end
+
+  def addition=(addition)
+    puts (@starting_number + @second_number)
+  end
+
+  def subtraction=(subtraction)
+    puts (@starting_number - @second_number)
+  end
+
+  def multiplication=(multiplication)
+    puts (@starting_number * @second_number)
+  end
+
+  def division=(division)
+    puts (@starting_number / @second_number)
+  end
+
+  def equals=(equals)
+  end
+
+  def clear=(clear)
+  end
+end
