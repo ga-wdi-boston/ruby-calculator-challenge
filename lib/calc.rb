992c9fe3779374d7d1acec6ec5c5d295c7ca275e
“newayliu1” <newayliu1@gmail.com>
2017-02-03 10:18:22 -0500

    Update
    - finished bonus question, method trains

--- lib/calc.rb
+++ lib/calc.rb
@@ -3,6 +3,7 @@
 # Calculator Class here
 class Calculator
   attr_reader :start_num
+  # attr_accessor :result
   def initialize(number)
     @start_num = number
     @result = number
@@ -10,18 +11,22 @@ class Calculator
 
   def add(number)
     @result = start_num + number
+    self
   end
 
-  def minus(number)
+  def subtract(number)
     @result = start_num - number
+    self
   end
 
-  def times(number)
+  def multiply(number)
     @result = start_num * number
+    self
   end
 
   def divide(number)
     @result = start_num.to_f / number
+    self
   end
 
   def equals
@@ -30,5 +35,6 @@ class Calculator
 
   def clear
     @result = 0
+    self
   end
 end
