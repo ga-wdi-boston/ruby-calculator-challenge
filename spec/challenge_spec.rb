8a9abab9048bc90486d8173f12bcf2b9f8d10ea2
Bernard Lee <bernardlee82@gmail.com>
2017-02-06 07:21:39 -0500

    Fix tests
    Fix tests involving arithmetic operations so that the absence of the
    method chaining bonus requirement doesn't cause them to fail.  Fix #40.

--- spec/challenge_spec.rb
+++ spec/challenge_spec.rb
@@ -40,11 +40,15 @@ RSpec.describe 'Requirements' do
       end
 
       it 'performs floating point addition' do
-        expect(@calculator.add(8).equals.class).to eq(Float)
+        @calculator.add(8)
+
+        expect(@calculator.equals.class).to eq(Float)
       end
 
       it 'adds values correctly' do
-        expect(@calculator.add(8).equals).to eq(11.0)
+        @calculator.add(8)
+
+        expect(@calculator.equals).to eq(11.0)
       end
     end
 
@@ -54,11 +58,15 @@ RSpec.describe 'Requirements' do
       end
 
       it 'performs floating point subtraction' do
-        expect(@calculator.subtract(4).equals.class).to eq(Float)
+        @calculator.subtract(4)
+
+        expect(@calculator.equals.class).to eq(Float)
       end
 
       it 'subtracts values correctly' do
-        expect(@calculator.subtract(4).equals).to eq(-1.0)
+        @calculator.subtract(4)
+
+        expect(@calculator.equals).to eq(-1.0)
       end
     end
 
@@ -68,11 +76,15 @@ RSpec.describe 'Requirements' do
       end
 
       it 'performs floating point multiplication' do
-        expect(@calculator.multiply(8).equals.class).to eq(Float)
+        @calculator.multiply(8)
+
+        expect(@calculator.equals.class).to eq(Float)
       end
 
       it 'multiplies values correctly' do
-        expect(@calculator.multiply(8).equals).to eq(24.0)
+        @calculator.multiply(8)
+
+        expect(@calculator.equals).to eq(24.0)
       end
     end
 
@@ -82,11 +94,15 @@ RSpec.describe 'Requirements' do
       end
 
       it 'performs floating point division' do
-        expect(@calculator.divide(2).equals.class).to eq(Float)
+        @calculator.divide(2)
+
+        expect(@calculator.equals.class).to eq(Float)
       end
 
       it 'divides values correctly' do
-        expect(@calculator.divide(2).equals).to eq(1.5)
+        @calculator.divide(2)
+
+        expect(@calculator.equals).to eq(1.5)
       end
     end
   end
