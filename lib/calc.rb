258ce07b3f05a593e80632b98871cbc9d2589b74
Danny Kirschner <rkirschner1377@gmail.com>
2017-04-13 17:17:42 -0400

    Updating solution branch
    Refresh for cohort LM01

--- lib/calc.rb
+++ lib/calc.rb
@@ -1,3 +1,37 @@
 # frozen_string_literal: true
 
-class Calculator; end
+# :nodoc:
+class Calculator
+  def initialize(initial_value)
+    @value = initial_value.to_f
+    self
+  end
+
+  def equals
+    @value
+  end
+
+  def clear
+    @value = 0.0
+  end
+
+  def add(value)
+    @value += value
+    self
+  end
+
+  def subtract(value)
+    @value -= value
+    self
+  end
+
+  def multiply(value)
+    @value *= value
+    self
+  end
+
+  def divide(value)
+    @value /= value
+    self
+  end
+end
