352de1d65321396c7145058fd2bdbdc92ce0a871
awoodrum87 <awoodrum87@gmail.com>
2017-04-14 13:32:15 -0400

    challenge for april 14

--- lib/calc.rb
+++ lib/calc.rb
@@ -1,3 +1,19 @@
 # frozen_string_literal: true
 
-class Calculator; end
+class Calculator
+  def initialize(start_num)
+    @start_num = start_num
+  end
+
+  def add(second_num)
+    @second_num = second_num
+    sum_numbers = @start_num + @second_num
+    puts "The sum of these numbers is #{sum_numbers}"
+  end
+
+  def sub(second_num)
+    @second_num = second_num
+    sub_numbers = @start_num - @second_num
+    puts "The difference between these numbers is #{sub_numbers}"
+  end
+end
