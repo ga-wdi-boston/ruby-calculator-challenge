23faf11523aa6ce21a0e89277b86953618cad590
jscohen <jon.cohen08@gmail.com>
2017-04-14 13:23:52 -0400

    Finished Calculator Challenge

--- lib/calc.rb
+++ lib/calc.rb
@@ -1,3 +1,41 @@
 # frozen_string_literal: true
 
-class Calculator; end
+#
+class Calculator
+  attr_accessor :value
+  def initialize(num)
+    @value = num
+  end
+
+  def add(num)
+    final = value + num.to_f
+    self.value = final
+    self
+  end
+
+  def equals
+    value.to_f
+  end
+
+  def clear
+    self.value = 0.0
+  end
+
+  def subtract(num)
+    final = value - num.to_f
+    self.value = final
+    self
+  end
+
+  def multiply(num)
+    final = value * num.to_f
+    self.value = final
+    self
+  end
+
+  def divide(num)
+    final = value / num.to_f
+    self.value = final
+    self
+  end
+end
