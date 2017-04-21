d89e2082a6d150d17b0b0127d003347252c5a228
Maggicorp <mvjcasey@gmail.com>
2017-04-21 09:12:28 -0400

    Challenge calculator
    Makes basic functions

--- lib/calc.rb
+++ lib/calc.rb
@@ -1,3 +1,49 @@
 # frozen_string_literal: true
 
-class Calculator; end
+class Calculator
+  attr_accessor :num
+
+def initialize(num)
+  @num = num
+end
+
+def add(var)
+  @num = num + var
+end
+
+def subtract(sub)
+  @num = num - sub
+end
+
+def multiply(mult)
+  @num = num * mult
+end
+
+def division(div)
+  @num = num.to_f/ div
+end
+
+def equals
+  @num = num
+end
+end
+
+puts cal1 = Calculator.new(2)
+
+puts cal1.equals
+
+p cal1.add(3)
+
+puts cal1.equals
+
+p cal1.subtract(3)
+
+puts cal1.equals
+
+p cal1.multiply(5)
+
+puts cal1.equals
+
+p cal1.division(3)
+
+puts cal1.equals
