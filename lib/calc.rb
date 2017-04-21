da0e698289b7513905298c42c32e58f54ac9e974
AlexMollohan <ajmollohan@gmail.com>
2017-04-21 09:21:06 -0400

    All requirements finished. Working on bonus.

--- lib/calc.rb
+++ lib/calc.rb
@@ -1,3 +1,32 @@
 # frozen_string_literal: true
 
-class Calculator; end
+class Calculator
+
+def initialize(start_num)
+  @num = start_num.to_f
+end
+
+def add(num)
+  @num += num
+end
+
+def subtract(num)
+  @num -= num
+end
+
+def multiply(num)
+  @num *= num
+end
+
+def divide(num)
+  @num /= num
+end
+
+def equals
+  @num.to_f
+end
+
+def clear
+  @num = 0.0
+end
+end
