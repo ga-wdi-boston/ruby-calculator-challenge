2ed603d1f5f16db8859e83fc7bd973c80a3dcb4f
yourUsername <joseph.belmonte@gmail.com>
2017-04-21 09:24:12 -0400

    Answers to challenge

--- lib/calc.rb
+++ lib/calc.rb
@@ -1,3 +1,32 @@
 # frozen_string_literal: true
 
-class Calculator; end
+class Calculator
+  def initialize(num)
+    @num = num.to_f
+  end
+
+def add(new_Num)
+  @num = @num + new_Num
+end
+
+def subtract(new_Num)
+  @num = @num - new_Num
+end
+
+def multiply(new_Num)
+  @num *= new_Num
+end
+
+def divide(new_Num)
+  @num = @num / new_Num
+end
+
+def equals
+  return @num
+end
+
+def clear
+  @num = 0
+end
+
+end
