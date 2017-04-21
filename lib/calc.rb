b8cfb6d3dc0b0a082a304b2e7e64e58af126d201
pjliddy <pjliddy@gmail.com>
2017-04-21 09:21:17 -0400

    Created calculator application in Ruby
    Performs all required calculations, returns floats, and allows chained
    methods. Passes all bin/rake tests.

--- lib/calc.rb
+++ lib/calc.rb
@@ -1,3 +1,41 @@
 # frozen_string_literal: true
 
-class Calculator; end
+# Calculator class with an initialize method that takes a starting number
+class Calculator
+  attr_reader :total
+  private :total
+
+  def initialize(num)
+    @total = num.to_f
+    self
+  end
+
+  def add(num)
+    @total += num
+    self
+  end
+
+  def subtract(num)
+    @total -= num
+    self
+  end
+
+  def multiply(num)
+    @total *= num
+    self
+  end
+
+  def divide(num)
+    @total /= num
+    self
+  end
+
+  def equals
+    @total
+  end
+
+  def clear
+    @total = 0.0
+    @total
+  end
+end
