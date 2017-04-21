bb3bd5dc92c4bc0e5c3a1c391bfe30b5b92f55df
rowlandhill <hill.rowland@gmail.com>
2017-04-21 09:38:51 -0400

    I know this is late, but I wanted to see if I could get a little feedback.
    I don't think I did it totally right, because it seems
    like I simplified it, or didn't fully follow the directions

--- lib/calc.rb
+++ lib/calc.rb
@@ -1,3 +1,31 @@
 # frozen_string_literal: true
 
-class Calculator; end
+class Calculator
+  def initialize(num)
+    @num = num
+  end
+
+  def addition(b)
+      puts @num + b
+  end
+
+  def subtraction(b)
+      puts @num - b
+  end
+
+  def multiplication(b)
+      puts @num * b
+  end
+
+  def division(b)
+      puts @num / b
+  end
+
+  def equals
+    puts @num
+  end
+
+  def clear
+    @num = 0
+  end
+end
