fecc2674c52f228b0eeb1de38ad28391c9d38112
AlexMollohan <ajmollohan@gmail.com>
2017-04-21 14:23:03 -0400

    Bonus attempt

--- lib/calc.rb
+++ lib/calc.rb
@@ -4,29 +4,36 @@ class Calculator
 
 def initialize(start_num)
   @num = start_num.to_f
+  self
 end
 
 def add(num)
   @num += num
+  self
 end
 
 def subtract(num)
   @num -= num
+  self
 end
 
 def multiply(num)
   @num *= num
+  self
 end
 
 def divide(num)
   @num /= num
+  self
 end
 
 def equals
   @num.to_f
+  self
 end
 
 def clear
   @num = 0.0
+  self
 end
 end
